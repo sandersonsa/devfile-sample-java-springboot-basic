8d3358b47a479e52225296c8e25b2a0591fdeb52
Sanderson Sa <sanderson@redhat.com>
2024-10-03 11:12:20 -0300

    add headers

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class DemoApplication {
 
+    private static final Log LOG = LogFactory.getLog(DemoApplication.class);
+
     @RequestMapping("/")
-    String home() {
+    String home(@RequestHeader Map<String, String> headers) {
+        headers.forEach((key, value) -> {
+            LOG.info(String.format("Header '%s' = %s", key, value));
+        });
         return "Hello World!";
     }
 
