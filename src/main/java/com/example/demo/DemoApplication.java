ce054355d3446e15d27f4ae5451b6342af132da0
Sanderson Sa <sanderson@redhat.com>
2024-10-10 13:06:58 -0300

    add POST method

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,11 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -30,6 +33,13 @@ public class DemoApplication {
             e.printStackTrace();
         }
         return "Hello World!";
+    }
+
+    @PostMapping(path = "/introspect", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public void test(@RequestParam Map<String, String> params) {
+        for (Map.Entry<?, ?> entry : params.entrySet()) {
+            System.out.printf("%-15s : %s%n", entry.getKey(), entry.getValue());
+       }
     }
 
     public static void main(String[] args) {
